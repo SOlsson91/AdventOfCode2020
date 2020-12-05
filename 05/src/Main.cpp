29843bac0385ab5b2d1fe99fec55f1fc30d30fa3
Sebastian Olsson <me@sebastianolsson.com>
2020-12-05 13:42:54 +0100

    Cleaned up the code

--- 05/src/Main.cpp
+++ 05/src/Main.cpp
@@ -4,62 +4,78 @@
 #include "Utility.h"
 #include "Timer.h"
 
+int GetRow(const std::string& line)
+{
+	int min = 0;
+	int max = 127;
+	for (unsigned long i = 0; i < line.size() - 3; i++)
+	{
+		if (i == line.size() - 4)
+		{
+			return (line[i] == 'F') ? min : max;
+		}
+		if (line[i] == 'F')
+		{
+			max = max - std::ceil((max - min) / 2.0f);
+		}
+		else if (line[i] == 'B')
+		{
+			min = min + std::ceil((max - min) / 2.0f);
+		}
+	}
+	return -1;
+}
+
+int GetColumn(const std::string& line)
+{
+	int min = 0;
+	int max = 7;
+	for (unsigned long i = line.size() - 3; i <= line.size(); i++)
+	{
+		if (i == line.size())
+		{
+			return (line[i] == 'L') ? min : max;
+		}
+		if (line[i] == 'L')
+		{
+			int temp = std::ceil((max - min) / 2.0f);
+			max = max - temp;
+		}
+		else if (line[i] == 'R')
+		{
+			int temp = std::ceil((max - min) / 2.0f);
+			min = min + temp;
+		}
+	}
+	return -1;
+}
+
 int PartOne(const std::vector<std::string>& data, std::vector<int>& seats)
 {
 	Timer t;
 	int highestId = 0;
 	for (auto line : data)
 	{
-		int min = 0;
-		int max = 127;
-		int row = 0;
-		for (unsigned long i = 0; i < line.size() - 3; i++)
+		int row = GetRow(line);
+		if (row == -1)
 		{
-			if (i == line.size() - 4)
-			{
-				row = (line[i] == 'F') ? min : max;
-				break;
-			}
-			if (line[i] == 'F')
-			{
-				int temp = std::ceil((max - min) / 2.0f);
-				max = max - temp;
-			}
-			else if (line[i] == 'B')
-			{
-				int temp = std::ceil((max - min) / 2.0f);
-				min = min + temp;
-			}
+			std::cout << "[Error] Could not get Row\n";
+			return -1;
 		}
 
-		int column = 0;
-		min = 0;
-		max = 7;
-		for (unsigned long i = line.size() - 3; i <= line.size(); i++)
+		int column = GetColumn(line);
+		if (column == -1)
 		{
-			if (i == line.size())
-			{
-				column = (line[i] == 'L') ? min : max;
-				break;
-			}
-			if (line[i] == 'L')
-			{
-				int temp = std::ceil((max - min) / 2.0f);
-				max = max - temp;
-			}
-			else if (line[i] == 'R')
-			{
-				int temp = std::ceil((max - min) / 2.0f);
-				min = min + temp;
-			}
+			std::cout << "[Error] Could not get Column\n";
+			return -1;
 		}
 
 		int result = (row * 8) + column;
 		seats.push_back(result);
+
 		if (result > highestId)
 			highestId = result;
 	}
-
 	return highestId;
 }
 
